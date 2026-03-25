ed12a7ffe869b72f4db2593d34001d5d4cbe409d
Eric <thominh25111997@gmail.com>
2026-03-26 02:20:16 +0700

    update-status

--- src/api/room/index.ts
+++ src/api/room/index.ts
@@ -1,3 +1,4 @@
+import { ROOM_STATUS_ENUM } from "@/enums/room-status.enum";
 import axiosClient from "../aixos.config";
 import type { IRoom } from "@/models/room";
 
@@ -5,7 +6,11 @@ import type { IRoom } from "@/models/room";
 
 
 const get = async (): Promise<IRoom[]> => {
-  return axiosClient.get('room/get')
+  return axiosClient.get('room/get', {
+    params: {
+      status: ROOM_STATUS_ENUM.HOAT_DONG
+    }
+  })
 };
 
 const getById = async (id: string): Promise<IRoom> => {
---
+++ src/enums/room-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ROOM_STATUS_ENUM {
+    HOAT_DONG = 'A',
+    BAO_TRI = 'M'
+}
